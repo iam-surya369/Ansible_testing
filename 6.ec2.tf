a53216a41894b5d9adc4e81991b8e6ce36df9cfb
iam-surya369 <suryakiran3596@gmail.com>
2024-11-17 01:51:10 +0530

    added terraform and ansible files

---
+++ 6.ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "webservers" {
+  count                       = length(var.instance_names)
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_pair
+  subnet_id                   = aws_subnet.public-subnets[count.index].id
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "${var.instance_names[count.index]}-server"
+  }
+}
